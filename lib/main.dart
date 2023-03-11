adeeedeb1dc97d306d610323aa639d5cbe4ccb68
moohat <refixzcorp@gmail.com>
2023-03-11 07:47:09 -0800

    invisble widget - Gridview

--- lib/main.dart
+++ lib/main.dart
@@ -14,95 +14,134 @@ class MyApp extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.deepPurple,
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.amber,
-                  child: Center(
-                    child: Text("Hallo"),
-                  )),
-              Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.red,
-                  child: Center(
-                    child: Text("Hallo"),
-                  )),
-              Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.green,
-                  child: Center(
-                    child: Text("Hallo"),
-                  )),
-              Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.amber,
-                  child: Center(
-                    child: Text("Hallo"),
-                  )),
-              Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.red,
-                  child: Center(
-                    child: Text("Hallo"),
-                  )),
-              Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.green,
-                  child: Center(
-                    child: Text("Hallo"),
-                  )),
-              Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.amber,
-                  child: Center(
-                    child: Text("Hallo"),
-                  )),
-              Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.red,
-                  child: Center(
-                    child: Text("Hallo"),
-                  )),
-              Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.green,
-                  child: Center(
-                    child: Text("Hallo"),
-                  )),
-              Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.amber,
-                  child: Center(
-                    child: Text("Hallo"),
-                  )),
-              Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.red,
-                  child: Center(
-                    child: Text("Hallo"),
-                  )),
-              Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.green,
-                  child: Center(
-                    child: Text("Hallo"),
-                  )),
-            ],
-          ),
+        body: GridView(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3, crossAxisSpacing: 10, mainAxisSpacing: 10),
+          padding: EdgeInsets.only(top: 10),
+          // mainAxisAlignment: MainAxisAlignment.center,
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 200,
+              height: 200,
+              color: Colors.amber,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+            ),
+            Container(
+                width: 100,
+                height: 100,
+                color: Colors.green,
+                child: Center(
+                  child: Text("Hallo"),
+                )),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.purpleAccent,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+            ),
+            Container(
+                width: 100,
+                height: 100,
+                color: Colors.green,
+                child: Center(
+                  child: Text("Hallo"),
+                )),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.amber,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+            ),
+            Container(
+                width: 100,
+                height: 100,
+                color: Colors.green,
+                child: Center(
+                  child: Text("Hallo"),
+                )),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.amber,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+            ),
+            Container(
+                width: 100,
+                height: 100,
+                color: Colors.green,
+                child: Center(
+                  child: Text("Hallo"),
+                )),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.amber,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+            ),
+            Container(
+                width: 100,
+                height: 100,
+                color: Colors.green,
+                child: Center(
+                  child: Text("Hallo"),
+                )),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.amber,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+            ),
+            Container(
+                width: 100,
+                height: 100,
+                color: Colors.green,
+                child: Center(
+                  child: Text("Hallo"),
+                )),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.amber,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+            ),
+            Container(
+                width: 100,
+                height: 100,
+                color: Colors.green,
+                child: Center(
+                  child: Text("Hallo"),
+                )),
+          ],
+          // ),
         ),
       ),
     );
