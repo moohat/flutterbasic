717cdb1f9f08bba9ba825c775aaa44f70fb8054f
moohat <refixzcorp@gmail.com>
2023-03-11 06:32:38 -0800

    membuat kolom di flutter- visible widget 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp())
+  runApp(MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        
+        appBar: AppBar(
+          title: Text("Judul"),
+          backgroundColor: Colors.deepPurple,
+        ),
+        body: Column(
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.amber,
+              child: Text("Halo"),
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.amber,
+              child: Text("Halo"),
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.amber,
+              child: Text("Halo"),
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.amber,
+              child: Text("Halo"),
+            ),
+          ],
+        ),
       ),
-    )
+    );
   }
 }
